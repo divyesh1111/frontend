2270cb3322a8e5c38d36c36ba9e5049ebb884d9b
KRISHNA BHARATBHAI MIROLIYA <krishnamiroliya05@gmail.com>
2023-05-25 12:05:29 +0530

    K

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,46 +5,41 @@ function Header() {
    
   return (
 
-    <div>
-        <div className='bg-black text-white text-2xl grid grid-cols-2 '>
-            <div className="font-bold py-5 px-5 flex ">
+    <div className='bg-black'>
+        <div className='bg-black text-white text-2xl flex grid-cols-6 ml-40 '>
+            <div className="font-bold py-5 px-5 flex bg-black hover:bg-gray-800">
                 
                 <FaHome className='mr-3  '  size='2rem'/>
                 HOME
             </div>
-            <ul className='flex gap-7 py-5 px-10 justify-end '>
-                
-                <li className='hover:font-bold '>
-                    Pages
-                </li>
-                <li className='hover:font-bold' >
-                    Products
-                </li>
-                <li className='hover:font-bold'>
-                    Cart
-                </li>
-                <li className='hover:font-bold'>
-                    About
-                </li>
-                <li className='hover:font-bold'>
-                    Sale
-                </li>
-            </ul>
-        </div>
-        <hr />
-        <div className='bg-black text-2xl grid grid-cols-2'>
-            <div>
-
+            <div  className="font-semibold py-5 px-5 flex bg-black hover:bg-gray-800"  >
+                Pages
             </div>
-            <div>
-                <ul className='flex  text-white gap-4 justify-end px-6 my-3'>
-                    <li>
-                        login
-                    </li>
-                    <li>
-                        signup
-                    </li>
-                </ul>
+            <div  className="font-semibold py-5 px-5 flex bg-black hover:bg-gray-800 " >
+            Products
+            </div>
+            <div   className="font-semibold py-5 px-5 flex bg-black hover:bg-gray-800 ">
+            Cart
+            </div>
+            <div   className="font-semibold py-5 px-5 flex bg-black hover:bg-gray-800 ">
+            About
+            </div>
+            <div  className="font-semibold py-5 px-5 flex bg-black hover:bg-gray-800 ">
+            Sale
+            </div>
+            
+           
+        </div>
+       
+        <div className='bg-black  text-2xl text-white gap-5 justify-end mr-96 flex grid-cols-3 '>
+            <div  className='bg-black py-5 px-5 hover:bg-gray-800' >
+                LOGIN 
+            </div>
+            <div  className='bg-black py-5 px-5 hover:bg-gray-800'>
+                SIGN UP 
+            </div>
+            <div className='py-5 px-5 '>
+            <input  classname='bg-gray-slate-500 '  type="text" class="prompt" placeholder="Search..." name="search" value="" />              
             </div>
         </div>
     </div>
